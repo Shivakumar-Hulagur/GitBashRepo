e9fafe9096e6b0d58ed2bdaf2d4e3579c49e667e
shivakumar-Hulagur <shivakumarhulagur9911@gmail.com>
2023-09-28 13:42:16 +0530

    first commit

---
+++ src/main/java/com/gitbash/genericutils/Demo.java
@@ -0,0 +1,11 @@
+package com.gitbash.genericutils;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+	@Test
+	public void demoTest()
+	{
+		System.out.println(" From demo test");
+	}
+}
---
+++ src/test/java/com/gitbash/customermodule/CreatCustomer.java
@@ -0,0 +1,12 @@
+package com.gitbash.customermodule;
+
+import org.testng.annotations.Test;
+
+public class CreatCustomer {
+
+	@Test
+	public void createCustomerTest()
+	{
+		System.out.println(" From create Customer Test ");
+	}
+}
