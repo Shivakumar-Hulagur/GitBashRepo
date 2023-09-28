6500f8932df7c00821ae1ae0e1740ee2c1aaef2a
shivakumarHulagur9911@gmail.com <shivakumarhulagur9911@gmail.com>
2023-09-28 15:35:54 +0530

    new commit

---
+++ src/test/java/com/gitbash/customermodule/ModifyCustomer.java
@@ -0,0 +1,12 @@
+package com.gitbash.customermodule;
+
+import org.testng.annotations.Test;
+
+public class ModifyCustomer {
+
+	@Test
+	public void modifyCustomerTest()
+	{
+		System.out.println(" From modify customer test");
+	}
+}
