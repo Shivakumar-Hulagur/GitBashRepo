142a51289a9e8fec973d36a3bccb53e95ac6050d
shivakumarHulagur9911@gmail.com <shivakumarhulagur9911@gmail.com>
2023-09-28 15:48:26 +0530

    admin1

---
+++ src/test/java/com/gitbash/customermodule/DeleteCustomer.java
@@ -0,0 +1,13 @@
+package com.gitbash.customermodule;
+
+import org.testng.annotations.Test;
+
+public class DeleteCustomer {
+
+	@Test
+	public void deleteCustomerTest()
+	{
+		System.out.println("--From delete Customer--");
+	}
+	
+}
