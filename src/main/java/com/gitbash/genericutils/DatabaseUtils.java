7abf8ec2e30573cdff542874cf46a5e51b6554ad
shivakumarHulagur9911@gmail.com <shivakumarhulagur9911@gmail.com>
2023-09-28 17:50:45 +0530

    5 th commit

--- src/main/java/com/gitbash/genericutils/DatabaseUtils.java
+++ src/main/java/com/gitbash/genericutils/DatabaseUtils.java
@@ -1,5 +1,12 @@
 package com.gitbash.genericutils;
 
+import org.testng.annotations.Test;
+
 public class DatabaseUtils {
 
+	@Test
+	public void getDataBaseConnection()
+	{
+		System.out.println(" From database connection ");
+	}
 }
