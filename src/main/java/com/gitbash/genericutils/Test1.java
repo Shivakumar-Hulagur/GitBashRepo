3d6305a80deac5e3c382ffaac8b504380dd4e12f
shivakumarHulagur9911@gmail.com <shivakumarhulagur9911@gmail.com>
2023-09-28 18:16:15 +0530

    Test

---
+++ src/main/java/com/gitbash/genericutils/Test1.java
@@ -0,0 +1,12 @@
+package com.gitbash.genericutils;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	@Test
+	public void getDataBaseConnection()
+	{
+		System.out.println(" Test ");
+
+}}
